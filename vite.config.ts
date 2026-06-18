f3dfb94e55c596151a9a189ff378438c54a52154
SELVA MEENAKSHI KANNAN <selvameenakshik@gmail.com>
2026-06-19 02:58:12 +0530

    Change base path from '/Ecopluse/' to '/Ecopulse2.0/'

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Ecopluse/',
+  base: '/Ecopulse2.0/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
